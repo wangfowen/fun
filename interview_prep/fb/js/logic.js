9d29a28054010856267ccd548814db985ce23229
Owen Wang <wangfowen@gmail.com>
2014-12-27 17:05:58 -0500

    more logic for fb

--- interview_prep/fb/js/logic.js
+++ interview_prep/fb/js/logic.js
@@ -1,9 +1,36 @@
 function layOutDay(events) {
-  //TODO: create evts from events
-  var evts = [
-    {left: 0, top: 100, height: 100, width: 100},
-    {left: 0, top: 300, height: 100, width: 100}
-  ];
+  var evts = [];
+
+  for (var i = 0; i < events.length; i++) {
+    var event = events[i];
+    var start = event.start;
+    var end = event.end;
+
+    if (end < start) {
+      console.log("End (" + end + ") can't be earlier than start (" + start + ")");
+      continue;
+    }
+
+    if (start < 0) {
+      console.log("Start (" + start + ") can't be earlier than 0 minutes from now");
+      start = 0;
+    }
+
+    if (end > 720) {
+      console.log("End (" + end + ") can't be later than 720 minutes from now");
+      end = 720;
+    }
+
+    //TODO: left, numOverlap
+    var numOverlap = 0;
+
+    evts.push({
+      left: 0,
+      top: start,
+      height: end - start,
+      width: 600 / (numOverlap + 1)
+    });
+  };
 
   React.render(React.createElement(Events, {events: evts}), document.getElementById('events'));
 }
