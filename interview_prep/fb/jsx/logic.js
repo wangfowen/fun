caf4e2fd09590870f81a87958907fe71c935499c
Owen Wang <wangfowen@gmail.com>
2014-12-28 11:03:59 -0500

    huzzah

--- interview_prep/fb/jsx/logic.js
+++ interview_prep/fb/jsx/logic.js
@@ -13,23 +13,44 @@ function layOutDay(events) {
 
     var W = 1;
 
-    _.each(eventsQueue, function(event, i, events) {
-      var currW = 1;
-      var laterIndex = i + 1;
+    var num = 0;
+    var latestInColumn = [];
 
-      while (laterIndex < events.length && events[laterIndex].start < event.end) {
-        currW++;
-        laterIndex++;
+    //figure out W by trying to place them in columns
+    _.each(eventsQueue, function(event) {
+      var matched = false;
+
+      for (var i = 0; i < latestInColumn.length; i++) {
+        if (!latestInColumn[num] || latestInColumn[num] <= event.start) {
+          matched = true;
+          latestInColumn[num] = event.end;
+          break;
+        } else {
+          num = (num + 1) % W;
+        }
       }
 
-      if (currW > W) {
-        W = currW;
+      //couldn't fit it in, so create a new column
+      if (!matched && latestInColumn.length != 0) {
+        W++;
+        num = W - 1;
       }
+      latestInColumn[num] = event.end;
     });
 
-    //TODO: how figure out which position is free?
-    var num = 0;
+    //place in the correct column
+    num = 0;
+    latestInColumn = [];
     _.each(eventsQueue, function(event) {
+      if (latestInColumn[num]) {
+        while (latestInColumn[num] && latestInColumn[num] > event.start) {
+          num = (num + 1) % W;
+        }
+
+      }
+
+      latestInColumn[num] = event.end;
+
       eventRenders.push({
         top: event.start,
         height: event.end - event.start,
@@ -95,7 +116,7 @@ layOutDay([{start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}
 //double chained overlaps - should be thirds
 //layOutDay([{start: 50, end: 100}, {start: 70, end: 120}, {start: 90, end: 140}, {start: 100, end: 120}, {start: 120, end: 130}]);
 //first and third and fifth touching - should be thirds
-//layOutDay([{start: 30, end: 100}, {start: 50, end: 200}, {start: 100, end: 150}, {start: 100, end: 170}, {start: 150, end: 200}]);
+//layOutDay([{start: 30, end: 100}, {start: 50, end: 300}, {start: 100, end: 150}, {start: 100, end: 170}, {start: 150, end: 200}]);
 //four in a row, no overlap - should be 1
 //layOutDay([{start: 50, end: 100}, {start: 100, end: 200}, {start: 200, end: 300}, {start: 300, end: 500}]);
 //two big ones, then two at back - should be 3
